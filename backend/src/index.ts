90c3c6be839e1ceda7097e146330b54839c53511
sieugene <sieugene@mail.ru>
2021-02-21 00:18:52 +0300

    logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import log4js from "log4js";
 require("dotenv").config();
 
 const app = express();
@@ -6,4 +7,12 @@ const port = process.env.PORT;
 app.get("/", (request, response) => {
   response.send("Hello world!");
 });
+
+const logger = log4js.getLogger();
+logger.level = process.env.LOG_LEVEL;
+
+logger.info("log4js log info");
+logger.debug("log4js log debug");
+logger.error("log4js log error");
+
 app.listen(port, () => console.log(`Running on port ${port}`));
