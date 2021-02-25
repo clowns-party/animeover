cb6efc7ffc62b06061f347e64984609774d07ab8
sieugene <sieugene@mail.ru>
2021-02-25 19:17:13 +0300

    add services

--- backend/src/controllers/auth/auth.controller.ts
+++ backend/src/controllers/auth/auth.controller.ts
@@ -10,17 +10,22 @@ import {
   Path,
   Body,
   Example,
+  Response,
   Query,
 } from "tsoa";
 
-// Functions
-import { FirebaseSignUp } from "./auth.functions";
+// Services
+import { AuthService } from "./authService";
 
 // Types
 import { ErrorSchema, UserSchema } from "./auth.schema";
 // Utils
 @Route("auth")
 export class AuthController extends Controller {
+  /**
+   *   User registration by email and password
+   *
+   */
   @Example({
     email: "test@gmail.com",
     password: "pas34Wo",
@@ -31,7 +36,34 @@ export class AuthController extends Controller {
     @Query() password: string
   ): Promise<UserSchema | ErrorSchema> {
     try {
-      const user = await FirebaseSignUp(email, password);
+      const user = new AuthService().signup(email, password);
+      if (user) {
+        this.setStatus(201);
+        return user;
+      } else {
+        this.setStatus(500);
+      }
+    } catch (error) {
+      this.setStatus(400);
+      return error;
+    }
+  }
+
+  /**
+   * Authorization of the user by email and password
+   *
+   */
+  @Example({
+    email: "test@gmail.com",
+    password: "pas34Wo",
+  })
+  @Post("/")
+  public async signin(
+    @Query() email: string,
+    @Query() password: string
+  ): Promise<UserSchema | ErrorSchema> {
+    try {
+      const user = new AuthService().signin(email, password);
       if (user) {
         this.setStatus(201);
         return user;
--- backend/src/controllers/auth/auth.functions.ts
+++ backend/src/controllers/auth/auth.functions.ts
@@ -1,5 +1,5 @@
 // Firebase
-import { User, firebaseAuth } from './../../firebase/index';
+import { User, firebaseAuth } from "./../../firebase/index";
 
 export const FirebaseSignUp = (email, password): Promise<User> => {
   return new Promise((resolve, reject) => {
@@ -21,6 +21,32 @@ export const FirebaseSignUp = (email, password): Promise<User> => {
         const errorMessage = error.message;
         reject({
           message: errorMessage ?? "required fields are not filled in",
+          code: errorCode ?? 400,
+        });
+      });
+  });
+};
+
+export const FirebaseSignIn = (email, password): Promise<User> => {
+  return new Promise((resolve, reject) => {
+    firebaseAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        if (user) {
+          resolve(user);
+        } else {
+          reject({
+            message: "invalid password or email address",
+            code: 500,
+          });
+        }
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        reject({
+          message: errorMessage ?? "invalid password or email address",
           code: errorCode ?? 400,
         });
       });
