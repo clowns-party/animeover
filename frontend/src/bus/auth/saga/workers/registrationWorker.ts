8c7f36cf2074725479c38aeb72a0f46fda24b827
raydim <snagin2004@gmail.com>
2021-03-11 23:30:16 +0300

    Make registration form

---
+++ frontend/src/bus/auth/saga/workers/registrationWorker.ts
@@ -0,0 +1,34 @@
+import { SagaIterator } from "redux-saga";
+import { put, call } from "redux-saga/effects";
+import { push } from "connected-react-router";
+import {
+  startFetching,
+  stopFetching,
+  setFetchingError,
+  set,
+} from "../../actions";
+import { signUpAsyncType } from "../../types";
+import { sendSignUpData } from "../../../../Services/api";
+
+export function* signUpWorker(action: signUpAsyncType): SagaIterator {
+  const { payload } = action;
+  try {
+    yield put(startFetching());
+    const result = yield call(sendSignUpData, payload);
+    if (result?.data) {
+      yield put(set(result.data));
+      yield put(push("/"));
+    } else {
+      yield put(
+        setFetchingError({
+          message: "unresolved",
+          code: "500",
+        })
+      );
+    }
+  } catch (error) {
+    yield put(setFetchingError(error?.response?.data ?? "Some error"));
+  } finally {
+    yield put(stopFetching());
+  }
+}
