bc6935de1aee0a8d82a78bf3229ee57bee444e90
sieugene <sieugene@mail.ru>
2021-03-17 12:55:51 +0300

    Rebase

--- frontend/src/bus/auth/hooks/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { AppState } from '../../../init/rootReducer';
-import { AuthState } from "../reducer";
-
-export const useAuth = (): AuthState => {
-  const { data, isFetching, error } = useSelector<AppState, AuthState>((state) => state.auth);
-
-  return { data, isFetching, error };
-};
