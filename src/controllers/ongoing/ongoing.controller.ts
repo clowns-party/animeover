7e2d5152f0bbe26b454a9171e7943072cfa83526
sieugene <sieugene@mail.ru>
2021-04-15 22:15:05 +0300

    Add get ongoing

--- src/controllers/ongoing/ongoing.controller.ts
+++ src/controllers/ongoing/ongoing.controller.ts
@@ -1,4 +1,4 @@
-import { Route, Get, Controller } from "tsoa";
+import { Route, Get, Controller, Path } from "tsoa";
 // Schemas
 import { AnimeList } from "../animedb/animedb.schema";
 // Service
@@ -6,10 +6,26 @@ import { OngoingService } from "./ongoingService";
 
 @Route("/ongoing")
 export class OngoingController extends Controller {
+  service: OngoingService;
+  constructor() {
+    super();
+    this.service = new OngoingService();
+  }
   @Get("/")
   public async getOngoing(): Promise<AnimeList> {
     try {
-      const ongoing = await new OngoingService().getOngoing();
+      const ongoing = await this.service.getOngoing();
+      this.setStatus(200);
+      return ongoing;
+    } catch (error) {
+      this.setStatus(400);
+      return error;
+    }
+  }
+  @Get("{ongoingId}")
+  public async getOneDetail(@Path() ongoingId: string) {
+    try {
+      const ongoing = await this.service.getOngoingById(ongoingId);
       this.setStatus(200);
       return ongoing;
     } catch (error) {
--- src/controllers/ongoing/ongoingService.ts
+++ src/controllers/ongoing/ongoingService.ts
@@ -1,6 +1,6 @@
 var fetch = require("node-fetch");
 // Schemas
-import { ShikimoriAnimeList } from "./ongoing.schema";
+import { ShikimoriAnimeItem, ShikimoriAnimeList } from "./ongoing.schema";
 import { AnimeSeason } from "./../animedb/animedb.schema";
 import { AnimeList, AnimeItem } from "./../animedb/animedb.schema";
 // Functions
@@ -9,8 +9,9 @@ import { setOngoingList, singleAnimeFormatter } from "./ongoing.functions";
 import { firestoreDB } from "./../../firebase/index";
 
 export class OngoingService {
-  private shikimoriURL: string =
-    "https://shikimori.one/api/animes?status=ongoing&season=2021&order=ranked&limit=10";
+  private shikimoriURL: string = "https://shikimori.one/api/";
+  private ongoing = "animes?status=ongoing&season=2021&order=ranked&limit=10";
+  private animes = "animes/";
   private season: AnimeSeason = "SPRING";
   private year: number = 2021;
   private interval = 86400000;
@@ -23,6 +24,30 @@ export class OngoingService {
         resolve(data);
       } catch (error) {
         reject(error);
+      }
+    });
+  }
+  public async getOngoingById(id: string): Promise<AnimeItem> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const fbData = await this.existOngoingInFB(id);
+        const data = !fbData ? await this.getShikimoriOngoingById(id) : fbData;
+        console.log("FIND IN", fbData ? "FIREBASE" : "SHIKI");
+        resolve(data);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  private async existOngoingInFB(id: string): Promise<AnimeItem> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const doc = await firestoreDB.collection("ongoing").doc(id);
+        const data = (await doc.get()).data();
+        resolve(data as AnimeItem);
+      } catch (error) {
+        resolve(undefined);
       }
     });
   }
@@ -107,13 +132,30 @@ export class OngoingService {
     );
   }
   // API SHIKIMORI
-  private async getShikimoriOngoing(): Promise<AnimeList> {
+  private async fetching(url: string) {
+    const jsondata = await fetch(url);
+    const text: string = await jsondata.text();
+    return text && JSON.parse(text);
+  }
+  private async getShikimoriOngoing(uri?: string): Promise<AnimeList> {
     return new Promise(async (resolve, reject) => {
       try {
-        const jsondata = await fetch(this.shikimoriURL);
-        const text: string = await jsondata.text();
-        const data: ShikimoriAnimeList = text && JSON.parse(text);
+        const url = !uri ? this.shikimoriURL + this.ongoing : uri;
+        const data: ShikimoriAnimeList = await this.fetching(url);
         resolve(this.toSingleFormat(data));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+  private getShikimoriOngoingById(id: string): Promise<AnimeItem> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const url = this.shikimoriURL + this.animes + id;
+        const data: ShikimoriAnimeItem = await this.fetching(url);
+        const format =
+          data && singleAnimeFormatter(data, this.season, this.year);
+        resolve(format);
       } catch (error) {
         reject(error);
       }
